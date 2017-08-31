eff286e48e3804e6b524697d93be938c1b5045f8
Borislav Nedev <borislav.nedev@abv.bg>
2017-08-31 09:20:01 +0300

    Create Web,Models and Data Projects.

---
+++ SportsSite/SportsSite.Data/SportsSystemDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace SportsSite.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+
+    public class SportsSiteDbContext : IdentityDbContext<User>
+    {
+        public SportsSiteDbContext()
+            : base("SportsSiteConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static SportsSiteDbContext Create()
+        {
+            return new SportsSiteDbContext();
+        }
+    }
+}
---
+++ SportsSite/SportsSite.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SportsSite.Web.MvcApplication" Language="C#" %>
