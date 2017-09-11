8f0a97688b5caa02dc6950f4dee0e5a8ac0b5eb5
Borislav Nedev <borislav.nedev@abv.bg>
2017-09-11 15:50:26 +0300

    Create BaseController.

---
+++ SportsSite/SportsSite.Web/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿namespace SportsSite.Web.Controllers
+{
+    using SportsSite.Data.UnitOfWork;
+    using System.Web.Mvc;
+
+    public class BaseController : Controller
+    {
+        protected ISportsSiteData Data;
+
+        public BaseController(ISportsSiteData data)
+        {
+            this.Data = data;
+        }
+
+        public BaseController()
+            : this(new SportsSiteData())
+        {
+        }
+    }
+}
---
+++ SportsSite/SportsSite.Web/Models/ArticleViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace SportsSite.Web.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class ArticleViewModel
+    {
+        public int Id { get; set; }
+        
+        public string Title { get; set; }
+        
+        public string Content { get; set; }
+
+        public string Date { get; set; }
+
+        public string ImageUrl { get; set; }
+
+    }
+}
